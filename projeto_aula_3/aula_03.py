26ebd2f2ef9e19cfb540814a2b1ead17a1da0837
atkonno <86972184+atkonno@users.noreply.github.com>
2021-07-05 14:49:27 -0300

    Add files via upload

---
+++ projeto_aula_3/aula_03.py
@@ -0,0 +1,12 @@
+def divide(a,b):
+    print(f"{a} dividido por {b} é igual a {a/b}.")
+
+def subtrai(a,b):
+    print(f"{a} menos {b} é igual a {a-b}.")
+
+def soma(a,b):
+    print(f"{a} mais {b} é igual a {a+b}.")
+
+divide(10,2)
+soma(10,2)
+subtrai(10,2)
